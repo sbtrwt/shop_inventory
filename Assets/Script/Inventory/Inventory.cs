e8172673fc7e646f92ca949d8671e973fa531126
shobhit-outscal <shobhit.rawat@outscal.com>
2024-02-27 18:33:07 +0530

    Shop and Inventory UI added

---
+++ Assets/Script/Inventory/Inventory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory 
+{
+    private List<Item> items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = new List<Item> { new Item {  }   };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Items/ItemObject.cs
+++ Assets/Script/Items/ItemObject.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemController 
+public abstract class ItemObject : ScriptableObject
 {
-
-   
-  
+    
 }
